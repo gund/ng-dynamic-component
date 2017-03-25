cba008d4ff1ba2b34c3524c3ca18bf787edc7cd0
gund <malkevich.alex@gmail.com>
2017-03-26 01:09:46 +0400

    fix(directive): Guard NgComponentOutlet that may be not injected

--- src/dynamic/dynamic.directive.ts
+++ src/dynamic/dynamic.directive.ts
@@ -33,7 +33,7 @@ export class DynamicDirective implements OnChanges, DoCheck, OnDestroy {
   private _destroyed$ = new Subject<void>();
 
   private get _compOutletInst(): any {
-    return (<any>this._componentOutlet)._componentRef;
+    return this._componentOutlet && (<any>this._componentOutlet)._componentRef;
   }
 
   private get _componentInst(): any {
