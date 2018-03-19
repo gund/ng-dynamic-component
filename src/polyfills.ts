5dbe13df3be5350ad230eca0c310447c3af14839
Alex Malkevich <malkevich.alex@gmail.com>
2018-03-19 15:58:17 +0000

    test(jest): Use jest for tests instead of karma

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,5 +1,6 @@
 // This file includes polyfills needed by Angular 2 and is loaded before
 // the app. You can add your own extra polyfills to this file.
+/*
 import 'core-js/es6/symbol';
 import 'core-js/es6/object';
 import 'core-js/es6/function';
@@ -14,6 +15,7 @@ import 'core-js/es6/regexp';
 import 'core-js/es6/map';
 import 'core-js/es6/set';
 import 'core-js/es6/reflect';
+*/
 
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
