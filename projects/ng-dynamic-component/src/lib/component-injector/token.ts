f7757dec30e13fbca086eb8851cceb422baa106a
Alex Malkevich <malkevich.alex@gmail.com>
2021-12-01 09:15:36 +0100

    fix(api): remove deprecated APIs
    BREAKING CHANGE: Removed APIs:
    - Removed `ComponentInjector` - use `DynamicComponentInjector`
    - Removed `COMPONENT_INJECTOR` - use `DynamicComponentInjectorToken`

--- projects/ng-dynamic-component/src/lib/component-injector/token.ts
+++ projects/ng-dynamic-component/src/lib/component-injector/token.ts
@@ -4,17 +4,6 @@ export interface DynamicComponentInjector {
   componentRef: ComponentRef<any> | null;
 }
 
-/**
- * @deprecated Since v6.0.0 - Use {@link DynamicComponentInjector} instead
- */
-export type ComponentInjector = DynamicComponentInjector;
-
 export const DynamicComponentInjectorToken = new InjectionToken<
   DynamicComponentInjector
 >('DynamicComponentInjector');
-
-/**
- * @deprecated Since v6.0.0 - Use {@link DynamicComponentInjectorToken} instead
- * and provide component class via `useExisting` instead of `useValue`
- */
-export const COMPONENT_INJECTOR = DynamicComponentInjectorToken;
